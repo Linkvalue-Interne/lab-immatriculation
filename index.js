b4e4b6c8efcf39df609367c371e19152ce8c55c8
oScape <benoit.chassignol@gmail.com>
2020-02-24 15:07:07 +0100

    install mysql

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
-const app = express()
+const mysql      = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'rpi',
+  password : 'raspberry'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
 
 app.listen(3000, function () {
   console.log('Server listening on port 3000!')
-})
+});
