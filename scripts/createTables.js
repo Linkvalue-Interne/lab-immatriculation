b2ed252ea8728fb384d79064e90a5a3f22ad364b
Benoit Chassignol <benoit.chassignol@gmail.com>
2020-02-24 17:18:14 +0100

    bootstrap the project

---
+++ scripts/createTables.js
@@ -0,0 +1,32 @@
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host : 'localhost',
+    database: 'pi',
+    user:     'pi',
+    password: 'raspberry'
+  }
+});
+
+knex.schema
+  .createTable('users', function(table) {
+    table.increments('id');
+    table.string('last_name');
+    table.string('first_name');
+  })
+  .createTable('license_plates', function(table) {
+    table.increments('id');
+    table.string('license_plate');
+    table
+      .integer('user_id')
+      .unsigned()
+      .references('users.id');
+  })
+  .createTable('fails', function(table) {
+    table.increments('id');
+    table.string('detected_license_plate');
+    table
+      .integer('license_plate_id')
+      .unsigned()
+      .references('license_plates.id');
+  });
