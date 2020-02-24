dcb7352547b9165ca969b294d292564ad81c1d6c
Geoffrey <geoffreycdias@gmail.com>
2020-02-24 17:21:41 +0100

    test the pi camera module

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const app = express();
+const PiCamera = require("pi-camera");
+// const express = require('express');
+// const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// });
+
+// app.listen(3000, function () {
+//   console.log('Server listening on port 3000!')
+// });
+
+const time = new Date().getTime();
+const camera = new PiCamera({
+  mode: "photo",
+  output: `/tmp/analyze/${time}.jpg`,
+  nopreview: true
 });
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000!')
-});
+camera.snap.then(() => console.log("tchick"));
