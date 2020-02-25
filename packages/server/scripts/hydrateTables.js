d38857b7a32539bf6b5fa6966ac56e2cf02e8e0d
oScape <benoit.chassignol@link-value.fr>
2020-02-25 14:38:14 +0100

    promisify all

--- packages/server/scripts/hydrateTables.js
+++ packages/server/scripts/hydrateTables.js
@@ -12,42 +12,47 @@ knex('users')
   .insert({
     last_name: "Lacagette",
     first_name: "Alexandre"
-  });
-knex('license_plates')
-  .insert({
-    license_plate: "AN154TS",
-    user_id: 1
+  }).then(() => {
+    knex('license_plates')
+      .insert({
+        license_plate: "AN154TS",
+        user_id: 1
+      });
+  }).then(() => {
+    knex('users')
+      .insert({
+        last_name: "Bender",
+        first_name: "Lars"
+      });
+  }).then(() => {
+    knex('license_plates')
+      .insert({
+        license_plate: "PL123AK",
+        user_id: 2
+      });
+  }).then(() => {
+    knex('users')
+      .insert({
+        last_name: "Olas",
+        first_name: "Jean-MiMi"
+      });
+  }).then(() => {
+    knex('license_plates')
+      .insert({
+        license_plate: "ZH522802",
+        user_id: 3
+      });
+  }).then(() => {
+    knex('users')
+      .insert({
+        last_name: "Gezaal",
+        first_name: "Rachid"
+      });
+  }).then(() => {
+    knex('license_plates')
+      .insert({
+        license_plate: "6324ZG77",
+        user_id: 4
+      });
   });
 
-knex('users')
-  .insert({
-    last_name: "Bender",
-    first_name: "Lars"
-  });
-knex('license_plates')
-  .insert({
-    license_plate: "PL123AK",
-    user_id: 2
-  });
-
-knex('users')
-  .insert({
-    last_name: "Olas",
-    first_name: "Jean-MiMi"
-  });
-knex('license_plates')
-  .insert({
-    license_plate: "ZH522802",
-    user_id: 3
-  });
-
-knex('users')
-  .insert({
-    last_name: "Gezaal",
-    first_name: "Rachid"
-  });
-knex('license_plates')
-  .insert({
-    license_plate: "6324ZG77",
-    user_id: 4
-  });
