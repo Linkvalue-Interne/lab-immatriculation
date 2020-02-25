708da0c89ab0e28ff2065577b6bec9d7ddad68fd
Geoffrey <geoffreycdias@gmail.com>
2020-02-25 14:20:06 +0100

    limit the number of results

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,7 +1,11 @@
 const { execSync } = require("child_process");
 const express = require("express");
 const cors = require("cors");
+
+const CONFIDENCE_MINIMAL_VALUE = 75;
+
 const app = express();
+
 const knex = require("knex")({
   client: "mysql2",
   connection: {
@@ -17,12 +21,15 @@ app.use(cors());
 app.get("/take/picture", function(req, res) {
   const time = new Date().getTime();
   execSync(`raspistill -o /tmp/analyze/${time}.jpg -t 300`);
-  let scan_picture = execSync(
-    `alpr -c eu --json /home/pi/Nouvelle_immatriculation_des_véhicules_de_la_police_française.jpg`,
+
+  const commandResult = execSync(
+    `alpr -c eu --json -n 1 /home/pi/Nouvelle_immatriculation_des_véhicules_de_la_police_française.jpg`,
     { encoding: "utf8" }
   );
+  const { results } = JSON.parse(commandResult);
+  const firstResult = results[0] || {};
 
-  res.json(scan_picture);
+  res.json(firstResult);
 });
 
 app.get("/plate/:id", function(req, res) {
