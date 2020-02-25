9e2af8e20ae143c86988681f573599304fb14ed4
Geoffrey <geoffreycdias@gmail.com>
2020-02-25 11:44:54 +0100

    add cors

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,32 +1,35 @@
 const { execSync } = require("child_process");
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const knex = require('knex')({
-  client: 'mysql2',
+const knex = require("knex")({
+  client: "mysql2",
   connection: {
-    host : 'localhost',
-    database: 'pi',
-    user:     'pi',
-    password: 'raspberry'
+    host: "localhost",
+    database: "pi",
+    user: "pi",
+    password: "raspberry"
   }
 });
 
-app.get('/take/picture', function (req, res) {
+app.use(cors());
+
+app.get("/take/picture", function(req, res) {
   const time = new Date().getTime();
   execSync(`raspistill -o /tmp/analyze/${time}.jpg -t 300`);
-  let scan_picture = execSync(`alpr /home/pi/Nouvelle_immatriculation_des_véhicules_de_la_police_française.jpg -c eu --json`);
+  let scan_picture = execSync(
+    `alpr /home/pi/Nouvelle_immatriculation_des_véhicules_de_la_police_française.jpg -c eu --json`
+  );
 
-  res.send(scan_picture);
+  res.json(scan_picture);
 });
 
-app.get('/plate/:id', function (req, res) {
+app.get("/plate/:id", function(req, res) {
   res.send(
-    knex
-      .select('license_plates')
-      .where('license_plate', '=', req.params.id)
+    knex.select("license_plates").where("license_plate", "=", req.params.id)
   );
 });
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000!')
+app.listen(3000, function() {
+  console.log("Server listening on port 3000!");
 });
