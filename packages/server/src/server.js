e992286762ffb88fc90fd8b10808e05c236fd8f2
oScape <benoit.chassignol@gmail.com>
2020-02-25 10:22:03 +0100

    first routes

---
+++ packages/server/src/server.js
@@ -0,0 +1,37 @@
+const { spawn } = require("child_process");
+const express = require('express');
+const app = express();
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host : 'localhost',
+    database: 'pi',
+    user:     'pi',
+    password: 'raspberry'
+  }
+});
+
+app.get('/take/picture', function (req, res) {
+  const time = new Date().getTime();
+  let child = spawn(`raspistill -o /tmp/analyze/${time}.jpg -t 300`);
+
+  child.stdout.on('data', (data) => {
+    console.log(`child stdout:\n${data}`);
+  });
+  
+  child.stderr.on('data', (data) => {
+    console.error(`child stderr:\n${data}`);
+  });
+});
+
+app.get('/plate/:id', function (req, res) {
+  res.send(
+    knex
+      .select('license_plates')
+      .where('license_plate', '=', req.params.id)
+  );
+});
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000!')
+});
