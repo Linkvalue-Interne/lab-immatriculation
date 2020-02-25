2c5d2cf12a416ce7ccfcdb37377f3d82b62977f9
Geoffrey <geoffreycdias@gmail.com>
2020-02-25 17:41:51 +0100

    add low confidence form

--- packages/client/src/views/LowConfidencePlate.vue
+++ packages/client/src/views/LowConfidencePlate.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
     <h1>Low confidence plate : {{ $route.params.plate }}</h1>
+    <v-form @submit.prevent="submitForm()">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-text-field
+              v-model="lowConfidencePlate"
+              label="Low Confidence License Plate"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="12">
+            <v-text-field
+              v-model="realPlate"
+              label="Real License Plate"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-btn color="blue" type="submit">Submit plate</v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      lowConfidencePlate: this.$route.params.plate,
+      realPlate: ""
+    };
+  },
+
+  methods: {
+    submitForm() {
+      alert(`${this.lowConfidencePlate} -> ${this.realPlate}`);
+    }
+  }
+};
 </script>
 
 <style></style>
