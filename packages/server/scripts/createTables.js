c687de7d73d3c6a7410efd13132f866f2b3e890b
Geoffrey <geoffreycdias@gmail.com>
2020-02-25 14:54:16 +0100

    hydrate tables

--- packages/server/scripts/createTables.js
+++ packages/server/scripts/createTables.js
@@ -1,32 +1,37 @@
-const knex = require('knex')({
-  client: 'mysql2',
+const knex = require("knex")({
+  client: "mysql2",
   connection: {
-    host : 'localhost',
-    database: 'pi',
-    user:     'pi',
-    password: 'raspberry'
+    host: "localhost",
+    database: "pi",
+    user: "pi",
+    password: "raspberry"
   }
 });
 
 knex.schema
-  .createTable('users', function(table) {
-    table.increments('id');
-    table.string('last_name');
-    table.string('first_name');
+  .createTable("users", function(table) {
+    table.increments("id");
+    table.string("last_name");
+    table.string("first_name");
   })
-  .createTable('license_plates', function(table) {
-    table.increments('id');
-    table.string('license_plate');
+  .createTable("license_plates", function(table) {
+    table.increments("id");
+    table.string("license_plate");
     table
-      .integer('user_id')
+      .integer("user_id")
       .unsigned()
-      .references('users.id');
+      .references("users.id");
   })
-  .createTable('fails', function(table) {
-    table.increments('id');
-    table.string('detected_license_plate');
+  .createTable("fails", function(table) {
+    table.increments("id");
+    table.string("detected_license_plate");
     table
-      .integer('license_plate_id')
+      .integer("license_plate_id")
       .unsigned()
-      .references('license_plates.id');
-  });
+      .references("license_plates.id");
+  })
+  .then(() => {
+    console.log("done");
+    process.exit(0);
+  })
+  .catch(err => console.error(err));
--- packages/server/scripts/hydrateTables.js
+++ packages/server/scripts/hydrateTables.js
@@ -1,58 +1,54 @@
-const knex = require('knex')({
-  client: 'mysql2',
+const knex = require("knex")({
+  client: "mysql2",
   connection: {
-    host : 'localhost',
-    database: 'pi',
-    user:     'pi',
-    password: 'raspberry'
+    host: "rpi",
+    database: "pi",
+    user: "pi",
+    password: "raspberry"
   }
 });
 
-knex('users')
-  .insert({
-    last_name: "Lacagette",
-    first_name: "Alexandre"
-  }).then(() => 
-    knex('license_plates')
-      .insert({
+knex("users")
+  .insert([
+    {
+      last_name: "Lacagette",
+      first_name: "Alexandre"
+    },
+    {
+      last_name: "Bender",
+      first_name: "Lars"
+    },
+    {
+      last_name: "Olas",
+      first_name: "Jean-MiMi"
+    },
+    {
+      last_name: "Gezaal",
+      first_name: "Rachid"
+    }
+  ])
+  .then(() =>
+    knex("license_plates").insert([
+      {
         license_plate: "AN154TS",
         user_id: 1
-      })
-  ).then(() => 
-    knex('users')
-      .insert({
-        last_name: "Bender",
-        first_name: "Lars"
-      })
-  ).then(() => 
-    knex('license_plates')
-      .insert({
+      },
+      {
         license_plate: "PL123AK",
         user_id: 2
-      })
-  ).then(() => 
-    knex('users')
-      .insert({
-        last_name: "Olas",
-        first_name: "Jean-MiMi"
-      })
-  ).then(() => 
-    knex('license_plates')
-      .insert({
+      },
+      {
         license_plate: "ZH522802",
         user_id: 3
-      })
-  ).then(() => 
-    knex('users')
-      .insert({
-        last_name: "Gezaal",
-        first_name: "Rachid"
-      })
-  ).then(() => 
-    knex('license_plates')
-      .insert({
+      },
+      {
         license_plate: "6324ZG77",
         user_id: 4
-      })
-  ).catch((err) => console.error(err));
-
+      }
+    ])
+  )
+  .then(() => {
+    console.log("done");
+    process.exit(0);
+  })
+  .catch(err => console.error(err));
