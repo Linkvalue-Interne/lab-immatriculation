6212e612c0b1b0177feb01d45bdf979e6c1e978c
Geoffrey <geoffreycdias@gmail.com>
2020-02-26 16:57:10 +0100

    add loading icon

--- packages/client/src/views/Home.vue
+++ packages/client/src/views/Home.vue
@@ -4,9 +4,10 @@
       v-if="user && !user.firstName"
       type="error"
     >No associated user with plate {{ user.licensePlate }}</v-alert>
-    <UserInformation v-else :user="user" />
+    <UserInformation v-if="user" :user="user" />
     <v-btn absolute dark fab bottom right color="blue" @click="analyze()">
-      <v-icon>mdi-video-input-antenna</v-icon>
+      <v-icon v-if="analyzing">mdi-timer</v-icon>
+      <v-icon v-else>mdi-video-input-antenna</v-icon>
     </v-btn>
   </div>
 </template>
@@ -24,18 +25,21 @@ export default {
   methods: {
     async analyze() {
       try {
+        this.analyzing = true;
         const user = await analyze();
-
         this.user = user;
+        this.analyzing = false;
       } catch (error) {
         this.$router.push("/low-confidence/" + error.plate);
+        this.analyzing = false;
       }
     }
   },
 
   data() {
     return {
-      user: null
+      user: null,
+      analyzing: false
     };
   }
 };
